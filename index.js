6e3e1c0bd13e4b6cc2b810437629007913313334
Dennis Mukuria <denismukuria@gmail.com>
2024-12-02 22:40:52 +0300

    add variables and assignment equations

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random()*10)+1;
+let num3 = 34;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1,4,6,8,10,20);
+
+
+
+
+
+
